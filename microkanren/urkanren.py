7e75810e91ac5a8a5d7af89e0132ab47856f1f26
Frank Berthold <fberthold@wordstream.com>
2017-10-31 17:49:33 -0400

    Added documentation and tests through eq.

--- microkanren/urkanren.py
+++ microkanren/urkanren.py
@@ -1,28 +1,46 @@
 def var(c):
+    """Var creates a Term by wrapping an integer in a list"""
     return [c]
 
 def varq(x):
+    """Determine if a value is a var"""
     return isinstance(x, list)
 
 def vareq(x1, x2):
+    """Determine if to vars are equal. This does not check if
+    they're values are the same, but if they are exactly the same
+    pointer."""
     return x1[0]  == x2[0]
 
-def walk(u, s):
-    pr = varq(u) and ([(variable, value) for (variable, value) in s if (vareq(u,variable))] or [False])[0]
+def walk(term, state):
+    """Walk through the given state.
+        If the term is not a variable, return it,
+        If the term is a variable and the first value found is not a variable, return it.
+        If the first value found is a variable, repeat walking on that variable."""
+    pr = varq(term) and ([(variable, value) for (variable, value) in state if (vareq(term,variable))] or [False])[0]
     if pr is not False:
-        return walk(pr[1], s)
+        return walk(pr[1], state)
     else:
-        return u
+        return term
 
-def ext_s(x, v, s):
-    return ([(x, v)] + s)
+def ext_s(variable, value, state):
+    """Add a value v to variable x for the given state s"""
+    return ([(variable, value)] + state)
 
 mzero = []
 
 def unit(soc):
+    """Don't change anything"""
     return [soc] + mzero
 
 def eq(u, v):
+    """Returns a function that takes a state/count object and returns
+        a new state/count object.
+       If u and v are both set terms, determines if they are the same.
+           If they are, returns the state as is.
+           If they are not, then returns an empty state.
+       If u or v is a variable, then asserts they are equal and adds it to,
+          the state. If they are not equal, then returns an empty state."""
     def eqHelp(soc):
         s = unify(u, v, soc[0])
         if s:
--- test/microkanren.py
+++ test/microkanren.py
@@ -1,10 +1,51 @@
+import os.path
+import sys
 import unittest
-from microkanren.urkanren import *
+from ..microkanren.urkanren import *
+
+empty_state = ([], 0)
+
+one_value = ([([0], 'hi')], 1)
+two_values = ([([0], 'hi'), ([1], 'bye')], 2)
+value_reference = ([([0], [1]), ([1], 'bye')], 2)
+identical_values = ([([0], 'hi'), ([1], 'hi')], 2)
 
 class Test_CoreMicroKanren(unittest.TestCase):
     def test_base_var(self):
         self.assertEqual(var(1), [1])
 
+    def test_base_varq(self):
+        self.assertTrue(varq(var(1)))
+
+    def test_neg_varq(self):
+        self.assertFalse(varq(1))
+
+    def test_base_vareq(self):
+        self.assertTrue(vareq(var(1), var(1)))
+
+    def test_base_vareq(self):
+        self.assertFalse(vareq(var(1), var(2)))
+
+    def test_base_case_walk(self):
+        self.assertEqual(walk(1, one_value[0]), 1)
+
+    def test_variable_case_walk(self):
+        self.assertEqual(walk([0], two_values[0]), 'hi')
+
+    def test_reference_walk(self):
+        self.assertEqual(walk([0], value_reference[0]), 'bye')
+
+    def test_base_ext_s(self):
+        self.assertItemsEqual(ext_s(var(1), 'bye', one_value[0]), two_values[0])
+
+    def test_literal_eq(self):
+        self.assertItemsEqual(eq(1, 1)(one_value), [one_value])
+
+    def test_literal_not_eq(self):
+        self.assertEqual(eq(1, 2)(one_value), [])
+
+    def test_literal_var_eq(self):
+        self.assertItemsEqual(eq([0], 'hi')(one_value), [one_value])
 
 if __name__ == "__main__":
     unittest.main()
